dfe12a8436f9afbd6a884cc446d42c63f3cd6f4a
Ricardo <ricardo.luo@icloud.com>
2021-05-19 21:04:51 +0800

    feat(deps): 新增支持tsx依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   base: './', // 设置打包路径
   server: {
     port: 3000, // 设置服务启动端口号
